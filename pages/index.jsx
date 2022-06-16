278c91effc300be6b347b8e68be8676ae6d70c35
steekam <waynewanyee@gmail.com>
2022-06-16 16:39:10 +0300

    completed page navigation and dealing with assets

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, I am <span>steekam</span> I solve problems using code. I am simply enjoying
+          where I am right now. You can find me on <a href="https://twitter.com/mauwanyee">Twitter</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
